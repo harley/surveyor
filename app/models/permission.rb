f5546e21eb5670c834356bcc916c5c69ab698a83
An Tran <tranminhan.my@gmail.com>
2016-07-30 18:31:09 +0700

    admin permission

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,15 +2,22 @@ class Permission < Struct.new(:user)
   delegate :role, to: :user, prefix: false
 
   LOOKUP = {
-    "admin" => ["surveys#new", "surveys#create", "surveys#edit", "surveys#update", "surveys#destroy", "surveys#show"],
-    "user" => ["surveys#new", "surveys#create", "surveys#edit", "surveys#update", "surveys#show"],
-    "owner" => ["surveys#new", "surveys#create"],
+    "admin" => %w(surveys#all responses#all collaborations#all),
+    "user" => %w(surveys#new surveys#create surveys#edit surveys#update surveys#show),
+    "owner" => %w(surveys#new surveys#create),
     "viewer" => ["surveys#show"],
     "editor" => ["surveys#edit"],
     "visitor" => ["surveys#show"]
   }
   def allow?(controller, action)
     right = "#{controller}##{action}"
-    LOOKUP[role] && LOOKUP[role].include?(right)
+    all_rights = "#{controller}#all"
+    god = "all#all"
+    LOOKUP[role] &&
+      (
+        LOOKUP[role].include?(right) ||
+        LOOKUP[role].include?(all_rights) ||
+        LOOKUP[role].include?(god)
+      )
   end
 end
